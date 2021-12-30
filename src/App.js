fbaedc25b9b7456d24ea97c820aa34d70937fa96
Yashar Akish <yashar_akish@yahoo.com>
2021-12-29 21:31:51 -0500

    using state from Component class and display state items in render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          id: 1,
+          name: "Frankenstein",
+        },
+        {
+          id: 2,
+          name: "Dracula",
+        },
+        {
+          id: 3,
+          name: "Zombi",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
-
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
+        }
+      </div>
     );
   }
 }
