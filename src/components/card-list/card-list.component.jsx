d604e9e00831ceddf8478679b3879d181b4a57c2
Yashar Akish <yashar_akish@yahoo.com>
2021-12-30 14:55:04 -0500

    using props in CardList instead of using props.childre and adjusting App.js by this modification

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,6 +5,6 @@ import './card-list.styles.css';
 export const CardList = props => {
 
   return <div className="card-list">
-    {props.children}
+    {props.monsters.map((monster)=> <h1 key={monster.id}>{monster.name}</h1>)}
   </div>
 }
