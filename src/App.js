aaee835d9dae18505a0f3d8ef39eced41017b39c
Yashar Akish <yashar_akish@yahoo.com>
2021-12-30 19:02:18 -0500

    creating SearchBox component with a input, also having a onChange handler with search functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
@@ -21,9 +23,21 @@ class App extends Component {
   }
 
   render() {
+
+    const {monsters, searchField} =  this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+
+        {/* <input type='search' placeholder='search monsters' 
+          onChange={ e => this.setState({searchField: e.target.value}) }/> 
+          // moving this part to SearchBox component then use SearchBox here // */}
+          
+        <SearchBox placeholder='search monsters'
+          handleChange={e => this.setState({searchField: e.target.value})}/>
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
